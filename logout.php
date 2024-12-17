edcbfd731db02d2c10435900a74651887c0423ff
mohamed102khaled <mohamed230105802@sut.edu.eg>
2024-12-17 07:35:57 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy(); // Destroy the session
-header('Location: login.html'); // Redirect to Sign-In page
+header('Location: index.html'); // Redirect to Sign-In page
 exit;
 ?>
