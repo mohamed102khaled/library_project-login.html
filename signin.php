31d0b2198c57188dfd6fc3b39eeae046d6925b06
mohamed102khaled <mohamed230105802@sut.edu.eg>
2024-12-17 07:38:10 +0200

    Update signin.php

--- signin.php
+++ signin.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($user) {
         // Check if the user's status is 'Inactive'
         if ($user['status'] === 'Inactive') {
-            echo "<script>alert('Your account is inactive. Please contact the admin.'); window.location.href = 'login.html';</script>";
+            echo "<script>alert('Your account is inactive. Please contact the admin.'); window.location.href = 'index.html';</script>";
             exit;
         }
 
@@ -30,10 +30,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
             exit;
         } else {
-            echo "<script>alert('Invalid username or password.'); window.location.href = 'login.html';</script>";
+            echo "<script>alert('Invalid username or password.'); window.location.href = 'index.html';</script>";
         }
     } else {
-        echo "<script>alert('Invalid username or password.'); window.location.href = 'login.html';</script>";
+        echo "<script>alert('Invalid username or password.'); window.location.href = 'index.html';</script>";
     }
 }
 ?>
